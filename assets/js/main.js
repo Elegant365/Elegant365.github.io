18f5b999c7829195ba4ef1ecef15f151f11e4931
Brandon Kong <shoukozumi@outlook.com>
2019-11-01 00:43:47 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,23 @@
 */
 
 (function($) {
+var $window = $(window);
+var $videoWrap = $('.video-wrap');
+var $video = $('.video');
+var videoHeight = $video.outerHeight();
 
+$window.on('scroll',  function() {
+  var windowScrollTop = $window.scrollTop();
+  var videoBottom = videoHeight + $videoWrap.offset().top;
+  
+  if (windowScrollTop > videoBottom) {
+    $videoWrap.height(videoHeight);
+    $video.addClass('stuck');
+  } else {
+    $videoWrap.height('auto');
+    $video.removeClass('stuck');
+  }
+});
 	var	$window = $(window),
 		$body = $('body');
 
